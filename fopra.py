5b8b0ea1903af5a5215c4eb5a9a2b5e26491591d
Fritz Schaal <schaalf@mathematik.uni-marburg.de>
2015-03-30 19:18:34 +0200

    bugfixes

---
+++ fopra.py
@@ -0,0 +1,274 @@
+import sys
+for folder in ["gather", "process", "view"]:
+	sys.path.append(folder)
+import OutputModule as outmod
+import StapModuleHandler as stmodh
+from time import sleep
+#from gi.repository import Gtk
+import gtk
+from threading import Thread
+
+
+class fopraMain():
+	def __init__(self, logMod = None, outMod = None):
+		self.mid = -1
+		self.lm = outmod.StdoutOutputModule()
+		self.tid = -1
+		try:
+			self.tid = int(sys.argv[1])
+		except IndexError, ValueError:
+			self.tid = -1
+		print "tid: %d" % self.tid
+			
+		# gui
+		self.initGTK()
+		
+		# log
+		self.lm = outmod.GtkListStoreOutputModule(self.builder.get_object("liststore5"))
+		self.om = outmod.GtkListStoreOutputModule(self.builder.get_object("liststore1"))
+		self.smh = stmodh.StapModuleHandler(outputStream=self.om,logStream=self.lm)
+
+		#self.gtkt = Thread(target=Gtk.main)
+		#self.gtkt = Thread(target=gtk.main)
+		self.gtkt = Thread(target=self.run)
+		self.gtkt.daemon = True
+
+	def initGTK(self):
+		self.gladefile = "view/stapLabGUI.glade"
+		#self.builder = Gtk.Builder()
+		self.builder = gtk.Builder()
+		self.builder.add_from_file(self.gladefile)
+
+		self.tidSelWin = self.builder.get_object("selectTidDialog")
+
+		self.mainWin = self.builder.get_object("mainWindow")
+		self.logWin = self.builder.get_object("logOutputWindow")
+
+		self.tidErrWin = self.builder.get_object("tidErrorWindow")
+		self.tidErrWin.hide()	
+
+		# wire events
+		self.tidErrWin.connect("delete-event", self.tidErrWinHide)
+		self.tidSelWin.connect("delete-event", self.stop)
+		self.mainWin.connect("delete-event", self.stop)
+		self.logWin.connect("delete-event", lambda w, e: w.hide() or True)
+		
+		# button wiring is done here rather than in the glade-file		
+		self.builder.get_object("button1").connect("clicked", self.showGraph)
+		self.builder.get_object("button2").connect("clicked", self.showFileChooserDialog)
+		self.builder.get_object("button3").connect("clicked", self.showRemoveModuleDialog)
+		self.builder.get_object("button4").connect("clicked", self.showAddModuleDialog)
+		self.builder.get_object("button5").connect("clicked", self.initGtkMainWindow)
+		self.builder.get_object("button6").connect("clicked", self.tidErrWinHide)
+		self.builder.get_object("button7").connect("clicked", self.dataExport)
+		self.builder.get_object("button8").connect("clicked", self.hideFileChooserDialog)
+		self.builder.get_object("button9").connect("clicked", self.hideAddModuleDialog,"dispatch")
+		self.builder.get_object("button10").connect("clicked", self.hideAddModuleDialog)
+		self.builder.get_object("button11").connect("clicked", self.hideRemoveModuleDialog,"delete")
+		self.builder.get_object("button12").connect("clicked", self.hideRemoveModuleDialog)
+		self.builder.get_object("button13").connect("clicked", self.showLogOutputWindow)
+	
+		# add autoscroll to all scrolledWindows
+		# TODO: to disable autoscroll, call these with "disconnect" instead of "connect"
+		self.builder.get_object("treeview1").connect("size-allocate", self.autoscroll, "treeview1")
+		self.builder.get_object("treeview2").connect("size-allocate", self.autoscroll, "treeview2")
+		self.builder.get_object("treeview3").connect("size-allocate", self.autoscroll, "treeview3")
+		self.builder.get_object("treeview4").connect("size-allocate", self.autoscroll, "treeview4")
+		self.builder.get_object("treeview5").connect("size-allocate", self.autoscroll, "treeview4")
+		
+		if self.tid > 0:
+			self.initGtkMainWindow()
+		else:
+			self.tidSelWin.show()
+			self.mainWin.hide()
+
+	def tidErrWinHide(self, *args):
+		self.tidErrWin.hide()
+
+	def initGtkMainWindow(self, *args):
+		self.max_pid_file = "/proc/sys/kernel/pid_max"
+		self.mainWin.connect("delete-event", self.stop)
+		try:
+			self.maxPID = int(open(self.max_pid_file).read().replace('\n',''))
+		except IOError as e:
+			print e
+		while True:	# there is no do-while in python, so we use a while True and a return statement further down
+			if self.tid > 0 and self.tid <= self.maxPID:
+				self.builder.get_object("entry1").set_text(str(self.tid))
+				self.mainWin.show_all()
+				self.tidSelWin.hide()
+				return self.tid
+			else:
+				self.builder.get_object("entry2").set_text(str(self.tid))
+				self.tidSelWin.show()
+				try:					
+					self.tid = int(self.builder.get_object("entry2").get_text())
+				except ValueError:
+					self.tid = -1
+					self.tidErrWin.show()
+
+	def showGraph(self,sm=None, *args):
+		self.builder.get_object("showGraphWindow").show_all()
+	
+	def showLogOutputWindow(self,*args):
+		self.builder.get_object("logOutputWindow").show()
+	
+	def showFileChooserDialog(self,*args):
+		self.builder.get_object("fileChooserDialog").show()
+	
+	def hideFileChooserDialog(self,*args):
+		self.builder.get_object("fileChooserDialog").hide()
+
+	def showRemoveModuleDialog(self,*args):
+		self.builder.get_object("removeModuleDialog").show()
+		#TODO
+		# set text accordingly to the selected module in the main window
+		pass
+
+	def hideRemoveModuleDialog(self,*args):
+		#TODO
+		self.builder.get_object("removeModuleDialog").hide()
+		if "remove" in args:
+			pass
+		self.updateSpinner()
+
+	def showAddModuleDialog(self,*args):
+		modules = []
+		for module in stmodh.stapModules.items():
+			modules += [module[0]]
+
+		self.updateListStore(ls=self.builder.get_object("liststore3"),values=modules)
+
+		self.addModWin = self.builder.get_object("addModuleDialog")
+		self.builder.get_object("treeview3").set_cursor(0)
+		self.addModWin.show()
+
+	def hideAddModuleDialog(self,*args):
+		self.builder.get_object("addModuleDialog").hide()
+		
+		if "dispatch" in args:
+			selectedModule = self.getSelectedRowFromTreeView(self.builder.get_object("treeview3"))
+			additionalArgs = self.builder.get_object("entry3").get_text()
+			self.log("dispatch: %s additional values:%s" % (selectedModule,additionalArgs))
+			additionalArgs = additionalArgs.split(",")
+			self.smh.startModule(module=selectedModule, args=additionalArgs, target=self.tid, outStream=self.om, logStream=self.lm)
+			#TODO recover old "show in output" state
+			ml = self.smh.getModuleList()
+			#print "ml: ", ml
+			vals = map((lambda m : [m[1].mname]+[True]),ml)
+			#vals = ['udp',True]
+			#vals = [["abc", True]]
+			#print "vals: ", vals
+			self.updateListStore(ls=self.builder.get_object("liststore2"),values=vals,clear=False)
+		self.updateSpinner()
+
+	def updateListStore(self,ls=None,values=[],clear=True,*args):
+		if ls is not None:
+			if clear:
+				ls.clear()
+			print "update, values: ", values
+			for val in values:
+				try:
+					#ls.append([val])
+					#it = ls.append(row=[val])
+					#print "append:" , val	
+					if isinstance(val,list):
+					#if ls is self.builder.get_object("liststore2"):
+					#	print "tv2"
+						it = ls.append(row=val)
+						#for e in [0,1]:
+						#	ls.set_value(it,e,val[e])
+					else:
+						it = ls.append(row=[val])
+					#ls.append(
+				except ValueError as e:
+					print "Exception:",e
+					self.stop()
+
+	def updateSpinner(self):
+		moduleCount = len(self.smh.getModuleList())
+		self.spinner = self.builder.get_object("spinner1")
+		if moduleCount > 0:
+			self.spinner.start()
+		else:
+			self.spinner.stop()
+
+	def autoscroll(self,sw=None,*args):
+		if sw is not None:
+			for elem in args:
+				if isinstance(elem,str) and elem.startswith("treeview"):	
+					tv = self.builder.get_object(elem)
+					adj = tv.get_vadjustment()
+   					adj.set_value( adj.get_upper() - adj.get_page_size() )
+					self.builder.get_object(elem).queue_draw()
+					return
+	
+	def getSelectedRowFromTreeView(self,treeView=None,*args):
+		if treeView is not None:
+			treeSelection = treeView.get_selection()
+			result = ""
+			(model, pathlist) = treeSelection.get_selected_rows()
+			for path in pathlist :
+				tree_iter = model.get_iter(path)
+				value = model.get_value(tree_iter,0)
+				result += value
+			return result
+		else:
+			return str(None)
+	
+	
+	def dataExport(self,*args):
+		# stapmodule = currently selected item in module list
+		# target = result of filechooserdialog, file/ db ...
+		sm = self.getSelectedRowFromTreeView(treeView=self.builder.get_object("treeview2"))
+		targetType = self.getSelectedRowFromTreeView(treeView=self.builder.get_object("treeview5"))
+		targetFile = self.builder.get_object("fileChooserDialog").get_filename()
+		print "Data export, module:", sm, " targetFile:", targetFile, " targetType:", targetType
+		self.builder.get_object("fileChooserDialog").hide()
+		pass
+	
+	def mainloop(self,*args):
+		#self.log("mainLoop Heartbeat %d" % self.c)
+		#print "hb",self.c
+		#gtk.main_iteration_do(blocking=False)
+		#self.c += 1
+		while gtk.events_pending():
+			gtk.main_iteration()
+		sleep(0.1)
+		return self.running	
+
+	def log(self,data):
+		self.lm.outLog((outmod.timestamp(),"fopraMain","-1",data))
+
+	def run(self):
+		try:
+			self.log("running()")
+			self.running = True
+			#self.gtkt.start()
+			self.c = 0
+			while self.mainloop():
+				pass
+		except KeyboardInterrupt:
+			self.stop()
+
+	def stop(self,*args):
+		self.lm = outmod.StdoutOutputModule()
+		self.om = self.lm
+		self.log("stop()")
+		if self.running:
+			self.running = False
+			self.smh.close()
+			self.mainWin.destroy()
+			#gtk.main_quit()
+			
+
+if __name__ == "__main__":
+	fm = fopraMain(logMod="stdout")
+	try:
+		fm.run()
+	except Exception as e:
+		print e
+	fm.stop()
+	sys.exit(0)
+
+
---
+++ process/OutputModule.py
@@ -0,0 +1,347 @@
+import threading as thr
+from Queue import Queue, Empty
+from time import sleep
+import datetime as dt
+import sys
+import sqlite3
+import os.path
+#from gi.repository import Gtk
+import gtk
+
+# global functions
+def timestamp():
+	return dt.datetime.now()
+
+def makeOutstreamTuple(module,data):
+	return (timestamp(),module.__class__.__name__, module.mid, data)
+
+def makeLogStr((timestamp, moduleName, moduleID, data)):
+	return "[LOG][%s][%s|%s][%s]" % (timestamp, moduleName, moduleID, data)
+
+def makeOutStr((timestamp, moduleName, moduleID, data)):
+	return "[OUT][%s][%s|%s][%s]" % (timestamp, moduleName, moduleID, data)
+
+Streams = { 'out':0, 'log':1 }
+
+# base class for all output streams
+# uses threads to asynchronously read the output-queues of all incomming streams
+# queues in python are threadsafe, see https://docs.python.org/2/library/queue.html
+class OutputModule():
+	def __init__(self,mID=-1):
+		self.qn		= Queue()			# normal out queue
+		self.ql		= Queue()			# log out queue
+		self.t 		= thr.Thread(target=self.run)	# 
+		self.t.daemon 	= True				#
+		self.running 	= True				#
+		self.t.start()					#
+		self.mid	= id(self) if mID is -1 else mID
+	
+	def enq(self,(timestamp,moduleName, moduleID, data)):
+		self.qn.put((timestamp,moduleName,moduleID,data))
+
+	def enqLog(self,(timestamp,moduleName, moduleID, data)):
+		self.ql.put((timestamp,moduleName,moduleID,data))
+
+	def deq(self):
+		try:
+			line = self.qn.get_nowait()
+		except Empty:
+			return None
+		return line
+
+	def deqLog(self):
+		try:
+			line = self.ql.get_nowait()
+		except Empty:
+			return None
+		return line	
+	
+	def out(self,(timestamp, moduleName, moduleID, data)=(timestamp(),"test",123,"data")):
+		#print "[%s][%s|%s][%s]" % (timestamp(),moduleName,moduleID,data)
+		print "data angekommen"
+
+	def outLog(self,(timestamp, moduleName, moduleID, data)=(timestamp(),"test",123,"data")):
+		print "logData angekommen"
+
+	def run(self):
+		while self.running:
+			l = self.deq()
+			while l is not None:
+				self.out(l)
+				l = self.deq()
+			l = self.deqLog()
+			while l is not None:
+				self.outLog(l)
+				l = self.deqLog()
+			sleep(0.1)
+
+	def close(self):
+		pass
+
+class DummyOutputModule(OutputModule):
+	def __init(self,mID=-1):
+		OutputModule.__init__(self,mID)
+
+	def out(self,(timestamp, moduleName, moduleID, data)):
+		pass
+
+	def outLog(self,(timestamp, moduleName, moduleID, data)):
+		pass
+
+class StdoutOutputModule(OutputModule):
+	def __init(self,mID=-1):
+		OutputModule.__init__(self,mID)
+		print makeLogStr(makeOutstreamTuple(self,data))
+
+	def out(self,(timestamp, moduleName, moduleID, data)):
+		print makeOutStr((timestamp, moduleName, moduleID, data))
+
+	def outLog(self,(timestamp, moduleName, moduleID, data)):
+		print makeLogStr((timestamp, moduleName, moduleID, data))
+
+# generate a global instance of stdout on import that anyone can grab.
+#gls = StdoutOutputModule()
+#gos = StdoutOutputModule()
+gls = DummyOutputModule()
+gos = DummyOutputModule()
+
+#global log Function
+def glog(module,data):
+	if gls is not None:
+		name = "None"
+		mid = -1
+		if module is not None:
+			name 	= module.__class__.__name__
+			mid	= module.mid
+		gls.outLog((timestamp(),name,mid,data))
+
+def gout(module,data):
+	if gos is not None:
+		name = "None"
+		mid = -1
+		if module is not None:
+			name 	= module.__class__.__name__
+			mid	= module.mid
+		gos.outLog((timestamp(),name,mid,data))
+
+
+class FileOutputModule(OutputModule):
+	def __init__(self,outFileName,mID=-1):
+		OutputModule.__init__(self,mID)
+		self.fn 	= outFileName
+		self.open()
+
+	def out(self,(timestamp, moduleName, moduleID, data)):
+		self.fh.write(makeOutStr(timestamp, moduleName, moduleID, data) + "\n")
+
+	def outLog(self,(timestamp, moduleName, moduleID, data)):
+		self.fh.write(makeLogStr(timestamp, moduleName, moduleID, data) + "\n")
+
+	def open(self):
+		self.fh = open(self.fn,"w")		# This opens the file and truncates it, if existent
+
+	def close(self):
+		self.fh.close()
+
+	def flush(self):
+		self.close()
+		self.open()
+
+# a thread with a reference to its' parent
+class prThread(thr.Thread):
+	def __init__(self,*args,**kwargs):
+		self.parent = None
+		thr.Thread.__init__(self,*args,**kwargs)
+
+class DatabaseOutputModule(OutputModule):
+	# DB Scheme:
+	# id
+	# stream Integer, 0 = outstream 1 = logstream, see "Streams" dict
+	# timestamp as text in YYYY-MM-DD hh:mm:ss:uuuuuu
+	# modulename 
+	# moduleid
+	# data raw data as text
+	
+	# autoFlush: creates Thread, that commits all data to file every <timeperiod>
+	def __init__(self, dbName,mID=-1,commitTimePeriod=5,minElementsFlush=10):
+		OutputModule.__init__(self,mID)
+		self.dbname 		= dbName
+		self.dbfn 		= self.dbname + ".sql"
+		self.con 		= None
+		self.ctp 		= commitTimePeriod	
+		self.ft 		= prThread(target=self.tFlush)
+		self.ft.parent 		= self
+		self.ft.daemon 		= True
+		self.lastFlush 		= dt.datetime.now()
+		self.minElemFlush 	= minElementsFlush	# write db to disk only after at 
+							# least this many elements are ready to be written
+		self.elemToFlush = 0			# how many elements are wating for a flush
+		self.init()
+	
+	def init(self):
+		if self.connect() == 0 :
+			self.createTable()
+		
+		if self.con is not None:
+			glog(self,"opened db on %s.sql" % self.dbname)
+			if self.ctp > 0:
+				self.ft.start()
+				self.lastFlush = dt.datetime.now()
+				glog(self,"threaded flushing started")
+			else:
+				glog(self,"using instant flushing")
+		else:
+			glog(self,"failed to open db on %s.sql" % self.dbname)
+		
+
+	def close(self):
+		if self.con is not None:
+			self.flush(force=True)
+			self.con.close()
+			self.con = None
+			glog(self,"shutdown db on %s.sql" % self.dbname)
+			
+
+	def connect(self):
+		if self.dbname is not None:
+			rval = 0
+			if os.path.isfile(self.dbfn):
+				glog(self,"DatabaseOutputModule fileName Exists: %s" % self.dbfn)
+				rval = 1
+			# check_same_thread=False -> allow commit triggered by other threads, too 
+			self.con = sqlite3.connect(self.dbfn,check_same_thread=False)
+			return rval
+		else:
+			glog(self,"DatabaseOutputModule needs valid db-name, None provided.")
+			return -1
+
+	def createTable(self):
+		if self.con is not None:
+			self.con.execute("""
+					CREATE TABLE %s (
+						id INTEGER PRIMARY KEY AUTOINCREMENT,
+						stream INTEGER,
+						timestamp TEXT,
+						moduleName TEXT,
+						moduleID TEXT,
+						data TEXT
+					)		
+				""" % (self.dbname))
+
+	def insert(self,(timestamp, moduleName, moduleID, data),stream=Streams['out']):
+		if self.con is not None:
+			insStr = "INSERT INTO %s (stream,timestamp,moduleName,moduleID,data) VALUES ( \'%s\',\'%s\',\'%s\',\'%s\',\'%s\')"
+			self.con.execute(insStr % (self.dbname,stream,timestamp, moduleName, moduleID, data))
+			#glog(self, "insert")
+			if self.ctp <= 0:
+				self.flush()
+			
+
+	def tFlush(self):
+		while True:
+			now = dt.datetime.now()
+			delta = (now - self.lastFlush).total_seconds()
+			if delta > self.ctp:
+				self.flush()
+				self.lastFlush = now
+			else:
+				sleep(delta)
+
+	def flush(self,force=False):
+		if self.elemToFlush > self.minElemFlush or force:		
+			self.con.commit()
+			self.elemToFlush = 0
+			glog(self,"commit")
+		else:
+			self.elemToFlush += 1
+	#	if self.con is not None:
+		
+		
+
+	def outLog(self,(timestamp, moduleName, moduleID, data)):
+		self.insert((timestamp, moduleName, moduleID, data),stream=Streams['log'])
+
+	def out(self,(timestamp, moduleName, moduleID, data)):
+		self.insert((timestamp, moduleName, moduleID, data),stream=Streams['out'])
+
+#TODO:
+# set a maximum number of lines to display in the target listStore
+# autoscroll to bottom of the corresponding treeview
+class GtkListStoreOutputModule(OutputModule):
+	def __init__(self,gtkListStore,mID=-1):
+		OutputModule.__init__(self,mID)
+		self.qn		= Queue()			# normal out queue
+		self.ql		= Queue()			# log out queue
+		self.t 		= thr.Thread(target=self.run)	# 
+		self.t.daemon 	= True				#
+		self.running 	= True				#
+		self.ls		= gtkListStore
+		self.t.start()					#
+		self.mid	= id(self) if mID is -1 else mID
+	
+	def enq(self,(timestamp,moduleName, moduleID, data)):
+		self.qn.put((timestamp,moduleName,moduleID,data))
+
+	def enqLog(self,(timestamp,moduleName, moduleID, data)):
+		self.ql.put((timestamp,moduleName,moduleID,data))
+
+	def deq(self):
+		try:
+			line = self.qn.get_nowait()
+		except Empty:
+			return None
+		return line
+
+	def deqLog(self):
+		try:
+			line = self.ql.get_nowait()
+		except Empty:
+			return None
+		return line	
+	
+	def out(self,(timestamp, moduleName, moduleID, data)=(timestamp(),"test",123,"data")):
+		data = makeOutStr((timestamp, moduleName, moduleID, data))
+		self.ls.append([data])
+
+	def outLog(self,(timestamp, moduleName, moduleID, data)=(timestamp(),"test",123,"data")):
+		data = makeLogStr((timestamp, moduleName, moduleID, data))
+		self.ls.append([data])
+
+#	def run(self):
+#		while self.running:
+#			l = self.deq()
+#			if l is not None:
+#				self.out(l)
+#			l = self.deqLog()
+#			if l is not None:
+#				self.outLog(l)
+
+	def close(self):
+		pass
+
+class OutputSplitterModule:
+	pass
+	
+
+if __name__ == "__main__":
+	#om = StdoutOutputModule()
+	#om = OutputModule()
+	#om = FileOutputModule("om.txt")
+	#om.enq("testmodule",1337,str(datetime.datetime.now()))
+	om = DatabaseOutputModule('test')
+	glog(om,"init done")
+	while True:
+		try:
+			sleep(1)
+			#om.enq((timestamp(),"testmodule",1337,str(datetime.datetime.now())))
+			om.out((timestamp(),"ftest",1337,"so much fake data"))
+			#sleepraise SystemExit
+			
+		except KeyboardInterrupt:
+			if isinstance(om,FileOutputModule):
+				om.close()
+				om.running = False
+			if isinstance(om,DatabaseOutputModule):
+				om.close()
+			sys.exit(0)
+
